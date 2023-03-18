bbb236255e0fee25b63b36f949e4ad3cf5c9c6ea
Ann Wangui Wairimu <ann99wangui@gmail.com>
2023-03-18 11:58:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messagesArray;
+  }
+  function countDown() {
+    let countDown = 0;
+    while(countDown<11) {
+        console.log(countDown++)
+    }
+  }
+  
